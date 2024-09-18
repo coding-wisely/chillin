62524233fe3ae4ecd183c04c47d82ed688f02357
Vladimir Nikolic <nezaboravi@gmail.com>
2024-09-18 16:55:06 +0200

    This is th message

--- app/Console/Commands/GitHelperCommand.php
+++ app/Console/Commands/GitHelperCommand.php
@@ -27,26 +27,30 @@ class GitHelperCommand extends Command
 
     protected function ensurePintInstalled(): void
     {
-        $this->ensureToolInstalled('vendor/bin/pint', 'Laravel Pint is not installed. Would you like to install it?', 'composer require laravel/pint --dev', 'Laravel Pint installed successfully.');
+        if (! file_exists(base_path('vendor/bin/pint'))) {
+            $installPint = confirm('Laravel Pint is not installed. Would you like to install it?', true);
+            if ($installPint) {
+                shell_exec('composer require laravel/pint --dev');
+                $this->info('Laravel Pint installed successfully.');
+            } else {
+                $this->error('Laravel Pint is required. Aborting.');
+                exit(1);
+            }
+        }
     }
 
     protected function ensurePestIsInstalled(): void
     {
-        $this->ensureToolInstalled('vendor/bin/pest', 'PHP Pest is not installed. Would you like to install it?', 'composer require pestphp/pest --dev --with-all-dependencies', 'PHP Pest installed successfully.', 'composer remove phpunit/phpunit', 'Removing PHP Unit...');
-    }
-
-    private function ensureToolInstalled(string $path, string $confirmMessage, string $installCommand, string $successMessage, string $preCommand = '', string $preMessage = ''): void
-    {
-        if (! file_exists(base_path($path))) {
-            if (confirm($confirmMessage, true)) {
-                if ($preCommand) {
-                    $this->info($preMessage);
-                    shell_exec($preCommand);
-                }
-                shell_exec($installCommand);
-                $this->info($successMessage);
+        if (! file_exists(base_path('vendor/bin/pest'))) {
+            $installPHPUnit = confirm('PHP Pest is not installed. Would you like to install it?', true);
+            if ($installPHPUnit) {
+                $this->info('Removing PHP Unit...');
+                shell_exec('composer remove phpunit/phpunit');
+                $this->info('Adding PHP Pest with all dependencies...');
+                shell_exec('composer require pestphp/pest --dev --with-all-dependencies');
+                $this->info('PHP Pest installed successfully.');
             } else {
-                $this->error("$confirmMessage is required. Aborting.");
+                $this->error('PHP Pest is required. Aborting.');
                 exit(1);
             }
         }
@@ -54,55 +58,65 @@ class GitHelperCommand extends Command
 
     protected function runPint(): array
     {
-        return $this->runTool('Laravel Pint', './vendor/bin/pint --dirty', '/^\s+✓ (\S+)/m');
+        $fixedFiles = [];
+        if (confirm('Would you like to run Laravel Pint?', true)) {
+            spin(
+                function () use (&$fixedFiles) {
+                    $pintOutput = shell_exec('./vendor/bin/pint --dirty');
+                    $this->info($pintOutput);
+                    // Collect the list of fixed files from the output
+                    preg_match_all('/^\s+✓ (\S+)/m', $pintOutput, $matches);
+                    if (isset($matches[1])) {
+                        $fixedFiles = $matches[1];
+                    }
+                    // Check if there were errors
+                    if (str_contains($pintOutput, 'FAIL') || str_contains($pintOutput, 'ERROR')) {
+                        $this->error('Laravel Pint found errors. Please fix them before proceeding.');
+                        exit(1); // Abort if there were errors
+                    }
+                },
+                'Running Laravel Pint...'
+            );
+            // Ensure we stage any fixed files right after Pint runs
+            foreach ($fixedFiles as $file) {
+                shell_exec("git add $file");
+                $this->info("Staged fixed file: $file");
+            }
+        } else {
+            $this->info('Skipping Laravel Pint.');
+        }
+
+        return $fixedFiles;
     }
 
     protected function runTests(): void
     {
-        $this->runTool('Pest Tests', './vendor/bin/pest', '', 'Fail', 'Error');
-    }
-
-    private function runTool(string $toolName, string $command, string $regexPattern = '', string $errorKeyword1 = 'FAIL', string $errorKeyword2 = 'ERROR'): array
-    {
-        $outputFiles = [];
-        if (confirm("Would you like to run $toolName?", true)) {
+        if (confirm('Would you like to run Pest Tests? If you need more detailed tests, please run it manually with flags you need.', true)) {
             spin(
-                function () use ($command, &$outputFiles, $regexPattern, $errorKeyword1, $errorKeyword2) {
-                    $output = shell_exec($command);
-                    $this->info($output);
-
-                    if ($regexPattern) {
-                        preg_match_all($regexPattern, $output, $matches);
-                        $outputFiles = $matches[1] ?? [];
-                    }
-
-                    if (str_contains($output, $errorKeyword1) || str_contains($output, $errorKeyword2)) {
-                        $this->error("$toolName found errors. Please fix them before proceeding.");
-                        exit(1);
+                function () {
+                    $testOutput = shell_exec('./vendor/bin/pest');
+                    $this->info($testOutput);
+                    // Check if there were errors
+                    if (str_contains($testOutput, 'Fail') || str_contains($testOutput, 'Error')) {
+                        $this->error('Your Pest tests failed. Please fix them before proceeding.');
+                        exit(1); // Abort if there were errors
                     }
                 },
-                "Running $toolName..."
+                'Running Pest Tests...'
             );
-
-            if ($regexPattern) {
-                foreach ($outputFiles as $file) {
-                    shell_exec("git add $file");
-                    $this->info("Staged fixed file: $file");
-                }
-            }
         } else {
-            $this->info("Skipping $toolName.");
+            $this->info('Skipping Pest Tests.');
         }
-
-        return $outputFiles;
     }
 
     protected function stageFixedFiles(array $fixedFiles): void
     {
+        // Stage the fixed files
         foreach ($fixedFiles as $file) {
             shell_exec("git add $file");
             $this->info("Staged fixed file: $file");
         }
+        // Check if there are any staged files
         $stagedFiles = shell_exec('git diff --cached --name-only');
         if (empty(trim($stagedFiles))) {
             $this->info('No changes staged for commit.');
@@ -111,6 +125,7 @@ class GitHelperCommand extends Command
 
     protected function commitChanges(): void
     {
+        // Ensure there are staged files
         $stagedFiles = shell_exec('git diff --cached --name-only');
         if (empty(trim($stagedFiles))) {
             $this->info('No changes staged for commit.');
@@ -120,30 +135,36 @@ class GitHelperCommand extends Command
         $commitMessage = text('Enter the commit message');
         if (! $commitMessage) {
             $this->error('Commit message cannot be empty.');
-            exit(1);
+            exit(1); // Abort if commit message is empty
         }
+        // Commit the staged files
         shell_exec("git commit -m \"$commitMessage\"");
         $this->info('Changes committed.');
-
-        $currentBranch = trim((new Process(['git', 'rev-parse', '--abbrev-ref', 'HEAD']))->run()->getOutput());
+        // Get the current branch name
+        $process = new Process(['git', 'rev-parse', '--abbrev-ref', 'HEAD']);
+        $process->run();
+        $currentBranch = trim($process->getOutput());
+        // Prefill branch name and add options
         $branch = $this->ask('Pushing code to', $currentBranch);
-
         while (! $branch) {
             $this->error('Branch name cannot be empty.');
             $branch = $this->ask('Pushing code to', $currentBranch);
         }
-
+        // Push the changes
         $process = new Process(['git', 'push', 'origin', $branch]);
         $process->run();
-
+        $this->info($process->getOutput());
         if ($process->isSuccessful()) {
             $this->info('Code pushed successfully.');
         } else {
             $this->error('Failed to push code.');
         }
-
-        $commitHash = trim(shell_exec('git log -1 --format="%H"'));
-        $gitUserName = trim(shell_exec('git config user.name'));
+        // Get the commit hash
+        $commitHash = shell_exec('git log -1 --format="%H"');
+        $commitHash = trim($commitHash);
+        // Get the Git username who pushed
+        $gitUserName = shell_exec('git config user.name');
+        $gitUserName = trim($gitUserName);
         $this->info("Commit hash: $commitHash");
         $this->info("Pushed by: $gitUserName");
     }
